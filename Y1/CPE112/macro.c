8502ccf43fa40f8a1cf504537b292f43a533c1e9
beambeambeam <supawitmarayat@gmail.com>
2025-01-28 18:15:25 +0700

    feat: macro lab

--- Y1/CPE112/macro.c
+++ Y1/CPE112/macro.c
@@ -6,4 +6,28 @@ typedef struct {
   int *items;
   size_t count;
   size_t capacity;
-} Numbers;
+} Array;
+
+int main(int argc, char const *argv[]) {
+  Array Numbers = {};
+  Numbers.count = 0;
+  Numbers.capacity = 10;
+  Numbers.items = (int *)malloc(Numbers.capacity * sizeof(int));
+  if (Numbers.items == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return 1;
+  }
+
+  REP(i, 0, 9) {
+    Numbers.items[i] = i + 1;
+    Numbers.count++;
+  }
+
+  REP(i, 0, Numbers.count - 1) {
+    printf("%d ", Numbers.items[i]);
+  }
+  printf("\n");
+
+  free(Numbers.items);
+  return 0;
+}
