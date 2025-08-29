af79052a42138401559e325591596c6bcd090e59
beambeambeam <supawitmarayat@gmail.com>
2025-08-29 11:29:33 +0700

    simplify greeting output in App class; remove getGreeting method

--- Y2/CPE231/cpe231-algo-2025/app/src/main/java/th/ac/kmutt/cpe/algorithm/supawit/App.java
+++ Y2/CPE231/cpe231-algo-2025/app/src/main/java/th/ac/kmutt/cpe/algorithm/supawit/App.java
@@ -4,11 +4,7 @@
 package th.ac.kmutt.cpe.algorithm.supawit;
 
 public class App {
-  public String getGreeting() {
-    return "Hello World! 2";
-  }
-
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+    System.out.println("hello world!");
   }
 }
