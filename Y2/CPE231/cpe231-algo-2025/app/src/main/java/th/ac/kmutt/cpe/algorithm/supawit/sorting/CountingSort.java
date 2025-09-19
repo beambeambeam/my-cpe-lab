e98e4a60c0216a1285ae097904b41b6b8b70fefe
beambeambeam <supawitmarayat@gmail.com>
2025-09-19 09:29:25 +0700

    Add CountingSort class implementing SortingStrategy for integer sorting

---
+++ Y2/CPE231/cpe231-algo-2025/app/src/main/java/th/ac/kmutt/cpe/algorithm/supawit/sorting/CountingSort.java
@@ -0,0 +1,81 @@
+package th.ac.kmutt.cpe.algorithm.supawit.sorting;
+
+import java.util.ArrayList;
+
+public class CountingSort<T extends Comparable<T>> implements SortingStrategy<T> {
+
+  public ArrayList<T> sorting(ArrayList<T> list) {
+    if (list == null || list.size() <= 1) {
+      return new ArrayList<>(list);
+    }
+
+    boolean allIntegers = true;
+    for (T item : list) {
+      if (item != null && !(item instanceof Integer)) {
+        allIntegers = false;
+        break;
+      }
+    }
+
+    if (!allIntegers) {
+      throw new UnsupportedOperationException("CountingSort only supports Integer types.");
+    }
+
+    return sortIntegerList(list);
+  }
+
+  private ArrayList<T> sortIntegerList(ArrayList<T> list) {
+    ArrayList<Integer> intList = new ArrayList<>();
+    for (T item : list) {
+      intList.add((Integer) item);
+    }
+
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+
+    for (Integer num : intList) {
+      if (num != null) {
+        min = Math.min(min, num);
+        max = Math.max(max, num);
+      }
+    }
+
+    if (min == Integer.MAX_VALUE) {
+      return new ArrayList<>(list);
+    }
+
+    int range = max - min + 1;
+    int[] count = new int[range];
+
+    for (Integer num : intList) {
+      if (num != null) {
+        count[num - min]++;
+      }
+    }
+
+    ArrayList<T> result = new ArrayList<>();
+
+    long nullCount = intList.stream().mapToLong(x -> x == null ? 1 : 0).sum();
+    for (int i = 0; i < nullCount; i++) {
+      result.add(null);
+    }
+
+    for (int i = 0; i < range; i++) {
+      int value = i + min;
+      for (int j = 0; j < count[i]; j++) {
+        result.add(findOriginalElement(list, value));
+      }
+    }
+
+    return result;
+  }
+
+  private T findOriginalElement(ArrayList<T> originalList, int value) {
+    for (T item : originalList) {
+      if (item != null && ((Integer) item).intValue() == value) {
+        return item;
+      }
+    }
+    return null;
+  }
+}
