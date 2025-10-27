235dc5e96726c3bc79dc3e282af6b2e86e9330e0
beambeambeam <supawitmarayat@gmail.com>
2025-10-27 17:15:23 +0700

    Add implementation for Presort Mode algorithm to find the mode of an array

--- Y2/CPE231/pratice/lecture/06-Transform-and-Conquer/P003_PresortMode.java
+++ Y2/CPE231/pratice/lecture/06-Transform-and-Conquer/P003_PresortMode.java
@@ -1,3 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P003_PresortMode {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] l = new int[n];
+    for (int i = 0; i < n; i++) {
+      l[i] = scanner.nextInt();
+    }
+
+    int mode = presortMode(l);
+    System.out.println(mode);
+
+    scanner.close();
+
+  }
+
+  private static int presortMode(int[] l) {
+    int modevalue = 0;
+    Arrays.sort(l);
+    int i = 0;
+    int modefrequency = 0;
+    while (i <= l.length - 1) {
+      int runlength = 1;
+      int runvalue = l[i];
+      while (i + runlength <= l.length - 1 && l[i + runlength] == runvalue) {
+        runlength++;
+      }
+      if (runlength > modefrequency) {
+        modefrequency = runlength;
+        modevalue = runvalue;
+      }
+      i = i + runlength;
+    }
+    return modevalue;
+  }
 
 }
