d7617161fa9e0edfe56aabb252b1a9c892fb8c1c
beambeambeam <supawitmarayat@gmail.com>
2025-10-27 17:07:51 +0700

    Add implementations for Unique Elements and Presort Element Uniqueness algorithms

--- Y2/CPE231/pratice/lecture/06-Transform-and-Conquer/P002_PresortElementUniquesness.java
+++ Y2/CPE231/pratice/lecture/06-Transform-and-Conquer/P002_PresortElementUniquesness.java
@@ -1,3 +1,28 @@
-public class P002_PresortElementUniquesness {
+import java.util.Scanner;
 
+public class P002_PresortElementUniquesness {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] l = new int[n];
+    for (int i = 0; i < n; i++) {
+      l[i] = scanner.nextInt();
+    }
+
+    boolean test = presort(l);
+    System.out.println(test);
+
+    scanner.close();
+
+  }
+
+  private static boolean presort(int[] l) {
+    for (int i = 0; i < l.length - 2; i++) {
+      if (l[i] == l[i + 1]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
